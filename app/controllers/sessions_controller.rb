ce34885e4a95c840071ef884e991a37fedf84316
Luis Puente <puente.luism@gmail.com>
2020-02-19 15:29:07 -0600

    Heroku deployment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,9 @@ class SessionsController < ApplicationController
   def show
     @clicked = params[:subject]
     @subjects = Subject.all
-    @start_session_link = Subject.find(@clicked)[:room]
+    if @clicked
+    @start_session_link = Subject.find_by_id(@clicked)[:room]
+    end
   end
 
   def index
